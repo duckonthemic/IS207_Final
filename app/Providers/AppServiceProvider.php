f8150faff678c523097eaa63d4f4a7f5b2a1a552
Hoang <hoang@example.com>
2025-12-12 14:21:14 +0700

    feat: Add AppServiceProvider to force HTTPS for Cloudflare Tunnel.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        // Force HTTPS for Cloudflare Tunnel
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
